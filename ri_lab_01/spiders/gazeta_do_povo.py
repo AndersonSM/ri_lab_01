6732a16521a94948bcabf6908e9430d12b1b2dce
AndersonSM <anderson.menezes@ccc.ufcg.edu.br>
2019-04-05 17:32:44 -0300

    resultado

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import datetime
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
@@ -21,10 +22,7 @@ class GazetaDoPovoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
 
-    def extract_data(self, response):
-        output = RiLab01Item()
-        self.id += 1
-        self.log('\n\n\n\nID: %s\n' % self.id)
+    def get_tile(self, response):
         # title
         title = response.xpath("//meta[@property='og:title']/@content")
         if len(title) > 0:
@@ -38,9 +36,13 @@ class GazetaDoPovoSpider(scrapy.Spider):
                 if len(title) > 0:
                     title = title[0].extract()
         self.log('TITLE: %s\n' % title)
-        if (title is None or len(title) == 0):
+        if title is None or len(title) == 0:
             title = '?'
 
+        return title
+
+
+    def get_desc(self, response):
         # desc
         desc = response.xpath("//meta[@property='og:description']/@content")
         if len(desc) > 0:
@@ -58,9 +60,13 @@ class GazetaDoPovoSpider(scrapy.Spider):
                     if len(desc) > 0:
                         desc = desc[0].extract()
         self.log('DESC: %s\n' % desc)
-        if (desc is None or len(desc) == 0):
+        if desc is None or len(desc) == 0:
             desc = '?'
 
+        return desc
+
+
+    def get_author(self, response):
         # author
         author = response.xpath("//meta[@name='article:author']/@content")
         if len(author) > 0:
@@ -70,9 +76,13 @@ class GazetaDoPovoSpider(scrapy.Spider):
             if len(author) > 0:
                 author = author[0].extract()
         self.log('AUTHOR: %s\n' % author)
-        if (author is None or len(author) == 0):
+        if author is None or len(author) == 0:
             author = 'Gazeta do Povo'
 
+        return author
+
+
+    def get_date(self, response):
         # date
         date = response.xpath("//meta[@property='article:published_time']/@content")
         if len(date) > 0:
@@ -82,9 +92,16 @@ class GazetaDoPovoSpider(scrapy.Spider):
             if len(date) > 0:
                 date = date[0].extract()
         self.log('DATE: %s\n\n\n\n' % date)
-        if (date is None or len(date) == 0):
+        if date is None or len(date) == 0:
             date = '?'
+        else:
+            date_obj = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+            date = date_obj.strftime("%d/%m/%Y %H:%M:%S")
 
+        return date
+
+
+    def get_section(self, response):
         # section
         section = response.xpath("//meta[@name='gp:section']/@content")
         if len(section) > 0:
@@ -94,17 +111,39 @@ class GazetaDoPovoSpider(scrapy.Spider):
             if len(section) > 0:
                 section = section[0].extract()
         self.log('SECTION: %s\n\n\n\n' % section)
-        if (section is None or len(section) == 0):
+        if section is None or len(section) == 0:
             section = '?'
 
+        return section
+
+
+    def get_text(self, response):
+        # text
+        text = ''.join(response.css('.paywall-google ::text').extract())
+        if len(text) == 0:
+            text = ''.join(response.css('.news-text ::text').extract())
+        if len(text) == 0:
+            text = ''.join(response.css('.texto-post ::text').extract())
+        self.log('TEXT: %s\n\n\n\n' % text)
+        if text is None or len(text) == 0:
+            text = '?'
+
+        return text
+
+
+    def extract_data(self, response):
+        output = RiLab01Item()
+        self.id += 1
+        self.log('\n\n\n\nID: %s\n' % self.id)
+
         output['_id'] = str(self.id)
-        output['title'] = title
-        output['sub_title'] = desc
-        output['author'] = author
-        output['date'] = date
-        output['section'] = section
+        output['title'] = self.get_tile(response)
+        output['sub_title'] = self.get_desc(response)
+        output['author'] = self.get_author(response)
+        output['date'] = self.get_date(response)
+        output['section'] = self.get_section(response)
+        output['text'] = self.get_text(response)
         output['url'] = response.url
-        output['text'] = '?'
 
         yield output
 
